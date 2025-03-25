f71b315321ce1ee3eefd8327196965a33e691ad4
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-25 11:05:08 +0000

    Hero Section & CV Builder Section

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="px-[5%] py-4 flex justify-between items-center bg-white">
+    <div class="text-2xl font-bold">
+        <a href="/">LeJob</a>
+    </div>
+    <div class="flex gap-8 items-center">
+        <a href="#" class="text-gray-800 hover:text-gray-600">Accueil</a>
+        <a href="#" class="text-gray-800 hover:text-gray-600">CV Builder</a>
+        <a href="#" class="text-gray-800 hover:text-gray-600">Offres</a>
+        <a href="#" class="text-gray-800 hover:text-gray-600">Booking</a>
+        <a href="#" class="bg-gray-800 text-white px-6 py-2 rounded-full hover:bg-gray-700">Je m'inscris</a>
+    </div>
+</nav>
