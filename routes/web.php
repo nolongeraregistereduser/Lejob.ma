00a89f040b313c54932e7b745d652672cf917898
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-25 22:13:14 +0000

    adding register page with responsive

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/contact', function () {
 Route::get('/create-cv', function () {
     return view('create-cv');
 });
+
+
+Route::get('/register', function () {
+    return view('register');
+});
