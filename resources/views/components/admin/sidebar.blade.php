f865abb8c46aa81001dd560c1a5824359765468d
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-05-01 23:04:04 +0100

    Adding feedback to admin sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -28,6 +28,12 @@
             </svg>
             Interviews
         </a>
+        <a href="{{ route('admin.feedback.index') }}" class="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-lg mb-1 {{ request()->routeIs('admin.feedback.*') ? 'bg-gray-800' : '' }}">
+            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
+            </svg>
+            Avis Clients
+        </a>
         <a href="#" class="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-lg mb-1">
             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
